8708a34533b62da064e9dc8ffc3ca1a6a837406a
georgedimitriadis <gdimitri@hotmail.com>
2022-12-31 15:00:10 +0000

    Basic Node functionality is up and running

--- Transforms/RL_Environments/Virtual_Rotation_Rat_Task/virtual_rotation_rat_task_worker.py
+++ Transforms/RL_Environments/Virtual_Rotation_Rat_Task/virtual_rotation_rat_task_worker.py
@@ -8,24 +8,32 @@ while path.split(current_dir)[-1] != r'Heron':
     current_dir = path.dirname(current_dir)
 sys.path.insert(0, path.dirname(current_dir))
 
+import numpy as np
 from Heron.communication.socket_for_serialization import Socket
 from Heron import general_utils as gu, constants as ct
 import commands_to_unity as cu
 
+
 screen_res = []
 observation_type: str
+translation_snap: float
+rotation_snap: int
 initialised = False
 
 
 def get_parameters(_worker_object):
     global screen_res
     global observation_type
+    global translation_snap
+    global rotation_snap
 
     try:
         parameters = _worker_object.parameters
         screen_res_x = parameters[0]
         screen_res_y = parameters[1]
-        observation_type = parameters[2]
+        translation_snap = parameters[2]
+        rotation_snap = parameters[3]
+        observation_type = parameters[4]
     except:
         return False
 
@@ -47,7 +55,7 @@ def initialise(_worker_object):
     if not cu.start_unity_exe(node_dir):
         return False
 
-    if not cu.first_communication_with_unity(screen_res):
+    if not cu.first_communication_with_unity(screen_res, translation_snap, rotation_snap):
         return False
 
     initialised = True
@@ -56,16 +64,23 @@ def initialise(_worker_object):
 
 def work_function(data, parameters, savenodestate_update_substate_df):
 
-
     topic = data[0]
 
     message = data[1:]  # data[0] is the topic
     message = Socket.reconstruct_array_from_bytes_message(message)
+    action_type = message[0].split(',')[0].replace(' ', '')
+    action_value = message[0].split(',')[1].replace(' ', '')
+
+    cu.do_action(action_type, action_value)
 
     #savenodestate_update_substate_df(image__shape=message.shape)
+    gu.accurate_delay(30)
+    result = cu.get_observation('Pixels')
 
-
-    result = [message]
+    if result is None:
+        result = [np.array(ct.IGNORE)]
+    else:
+        result = [np.ascontiguousarray(result)]
 
     return result
 
